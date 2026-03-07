710c4bf8680401b718dbe6305788ea81fa65a7fb
daguri <daguri75@gmail.com>
2026-03-07 11:23:09 +0700

    2026030704

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import { createApp } from "./app.js";
 import { config } from "./config.js";
 import { logger } from "./utils/logger.js";
+import { runListenerLoop } from "./chain/listener.js";
 
 const app = createApp();
 
 const server = app.listen(config.port, () => {
   logger.info({ port: config.port }, "jackpot api started");
+  runListenerLoop().catch((err) => {
+    logger.error({ err }, "listener fatal error");
+    process.exit(1);
+  });
 });
 
 function shutdown(signal) {
